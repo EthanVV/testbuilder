7d3a6cb6b9e617a87f29b3972e058c83cf8734e3
EthanVV <ethan.vanvleck@gmail.com>
2018-09-10 17:43:17 -0700

    Cards detected up to 'Maestro'

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,18 +1,23 @@
-// Given a credit card number, this function should return a string with the 
-// name of a network, like 'MasterCard' or 'American Express'
-// Example: detectNetwork('343456789012345') should return 'American Express'
-
-// How can you tell one card network from another? Easy! 
-// There are two indicators:
-//   1. The first few numbers (called the prefix)
-//   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
+  var network = '';
+  var length = cardNumber.length;
+  var first = function(depth) {
+    return cardNumber.slice(0,depth);
+  };
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  if ((first(2) === '38' || first(2) === '39') && length === 14) {
+    network = 'Diner\'s Club';
+  } else if ((first(2) === '34' || first(2) === '37') && length === 15) {
+    network = 'American Express';
+  } else if (first(1) === '4' && (length === 13 || length === 16 || length === 19)) {
+    network = 'Visa';
+  } else if (first(2) >= '51' && first(2) <= '55' && length === 16) {
+    network = 'MasterCard';
+  } else if ((first(4) === '6011' || (first(3) >= '644' && first(3) <= '649') || first(2) === '65') && (length === 19 || length === 16)) {
+    network = 'Discover';
+  } else if ((first(4) === '5018' || first(4) === '5020' || first(4) === '5038' || first(4) === '6304') && length >= 12 && length <= 19) {
+    network = 'Maestro';
+  }
+  return network;
 };
-
-
